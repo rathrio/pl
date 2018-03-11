c470b154e13f8c6163760392abce9387f7878cce
Rathesan Iyadurai <rad.iyadurai@gmail.com>
2018-03-11 16:37:49 +0100

    Add exercise 3

--- Assignment03/assignment03.hs
+++ Assignment03/assignment03.hs
@@ -5,15 +5,15 @@ factorial n = product [1..n]
 catalan n = factorial (2 * n) / (factorial (n + 1) * factorial n)
 firstNCatalan n = map catalan [0..n]
 
-
 -- Exercise 2
 divisors n = filter (\x -> n `mod` x == 0) [1..n]
 isPerfectNumber n = (sum (divisors n)) `div` 2 == n
 perfectNumbers n m = filter isPerfectNumber [(n + 1)..(m - 1)]
 
-
 -- Exercise 3
-insert i n l = undefined
+insert i n l = do
+  l1 ++ [n] ++ l2
+  where (l1, l2) = splitAt i l
 
 -- Exercise 4
 indexes n l = undefined
@@ -29,8 +29,8 @@ tests = TestList [
   (TestCase (assertEqual "not correct" [6, 28] (perfectNumbers 1 29 ))),
 
   -- exercise 3
-  -- (TestCase (assertEqual "not correct" [1, 2, 3, 4, 5] (insert 0 1 [2..5]))),
-  -- (TestCase (assertEqual "not correct" [2, 3, 4, 5, 1] (insert 7 1 [2..5]))),
+  (TestCase (assertEqual "not correct" [1, 2, 3, 4, 5] (insert 0 1 [2..5]))),
+  (TestCase (assertEqual "not correct" [2, 3, 4, 5, 1] (insert 7 1 [2..5]))),
 
   -- exercise 4
   -- (TestCase (assertEqual "not correct" [] (indexes 1 [0,0,0]))),
