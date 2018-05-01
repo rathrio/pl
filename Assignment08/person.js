c9f470fb9eb235ec41c824dc2c9dc54ea905f692
Rathesan Iyadurai <rad.iyadurai@gmail.com>
2018-05-01 16:35:00 +0200

    Add exercise 1

---
+++ Assignment08/person.js
@@ -0,0 +1,51 @@
+var Person = function(name, password) {
+  // Name is a property on Person instances.
+  this.name = name;
+
+  // Password won't be exposed as a property. It's only a local variable here.
+  // We'll expose it with the getter below.
+  var password = password;
+
+  this.getPassword = function() {
+    return password;
+  }
+
+  // The counter returned is a property defined on the constructor function
+  // itself. This is one way to "share" a value.
+  this.getCounter = function() {
+    return Person.counter;
+  }
+
+  // This will increment the shared value for demo purposes.
+  this.incCounter = function() {
+    Person.counter += 1;
+  }
+}
+
+Person.counter = 0;
+
+console.log("Creating a person instance p1 with name 'Jack' and password '123456'");
+var p1 = new Person("Jack", "123456");
+
+console.log("Creating a person instance p2 with name 'Jill' and password 'foobar'");
+var p2 = new Person("Jill", "foobar");
+
+console.log("\nDemostrating that the name property is accessible");
+console.log("p1's name: " + p1.name);
+console.log("p2's name: " + p2.name);
+
+console.log("\nDemostrating that the password property is not accessible, i.e. undefined");
+console.log("p1's password: " + p1.password);
+console.log("p2's password: " + p2.password);
+
+console.log("\nDemostrating that the password could be accessed via a getter");
+console.log("p1's password via a getter: " + p1.getPassword());
+console.log("p2's password via a getter: " + p2.getPassword());
+
+console.log("\nDemostrating that counter is shared");
+console.log("p1.getCounter(): " + p1.getCounter());
+console.log("p2.getCounter(): " + p2.getCounter());
+console.log("Calling p1.incCounter()");
+p1.incCounter();
+console.log("p1.getCounter(): " + p1.getCounter());
+console.log("p2.getCounter(): " + p2.getCounter());
