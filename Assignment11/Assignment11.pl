8bbe862556b060f6b7642ed80f623626749e83bf
Rathesan Iyadurai <rad.iyadurai@gmail.com>
2018-05-24 21:44:47 +0200

    Handle optional objects

--- Assignment11/Assignment11.pl
+++ Assignment11/Assignment11.pl
@@ -1,23 +1,22 @@
-sentence --> subject, predicate.
-sentence --> subject, predicate, object.
+sentence --> subject, predicate(without_oject).
+sentence --> subject, predicate(with_object), object.
 
 subject --> article, noun.
 subject --> pronoun.
 
-% predicate --> auxiliary.
-predicate --> verb.
+predicate(without_oject) --> [sleeps].
+predicate(with_object) --> [likes].
+predicate(with_object) --> [is].
 
 object --> article, noun.
 
 article --> [a].
 article --> [the].
+article --> [that].
+article --> [this].
 
 pronoun --> [this].
 pronoun --> [that].
 
 noun --> [boy].
 noun --> [girl].
-
-verb --> [likes].
-verb --> [is].
-verb --> [sleeps].
