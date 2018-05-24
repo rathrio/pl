11f1087620de6a3485ee68bbde54e1dd78b89685
Rathesan Iyadurai <rad.iyadurai@gmail.com>
2018-05-24 21:29:33 +0200

    Add stupid version

---
+++ Assignment11/Assignment11.pl
@@ -0,0 +1,23 @@
+sentence --> subject, predicate.
+sentence --> subject, predicate, object.
+
+subject --> article, noun.
+subject --> pronoun.
+
+% predicate --> auxiliary.
+predicate --> verb.
+
+object --> article, noun.
+
+article --> [a].
+article --> [the].
+
+pronoun --> [this].
+pronoun --> [that].
+
+noun --> [boy].
+noun --> [girl].
+
+verb --> [likes].
+verb --> [is].
+verb --> [sleeps].
